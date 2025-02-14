0d0b33e9a2f43305d2f231d04e9c09aea08007c8
TienPhamDev <manhtienbn18891@gmail.com>
2025-02-13 20:29:52 -0500

    cm

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import iconMenu from '/icon-menu.svg'
-import { setupCounter } from './counter.js'
-import logo from '/logo.svg';
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src =${logo}>
-  </div>
-`
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import iconMenu from "/icon-menu.svg";
+import { setupCounter } from "./counter.js";
+import logo from "/logo.svg";
+document.querySelector("#app").innerHTML = `
+  <header>
+    <nav>
+      <button class="menuMyDoc">
+        <svg width="30" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h30v2H0zM0 8h30v2H0zM0 16h30v2H0z"/></g></svg>
+      </button>
+    </nav>
+  </header>
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
