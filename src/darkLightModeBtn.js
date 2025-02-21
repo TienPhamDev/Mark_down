f1378a54a2f2f228fac85ad5e6f454ad77726196
TienPhamDev <manhtienbn18891@gmail.com>
2025-02-21 00:16:45 -0500

    done code function for button dark light mode

--- src/darkLightModeBtn.js
+++ src/darkLightModeBtn.js
@@ -1,12 +1,22 @@
 export const darkLightModeBtn = (element) => {
   element.addEventListener("click", () => {
     const bodyNode = document.body;
+    const darkLightDiv = document.querySelector(".darkLightModeDiv");
+
     const spanElement = element.querySelector("span");
     if (bodyNode.classList.contains("darkMode")) {
       bodyNode.classList.remove("darkMode");
+      darkLightDiv.lastElementChild.firstElementChild.style.fill =
+        "var(--slate-200)";
+      darkLightDiv.firstElementChild.firstElementChild.style.fill =
+        "var(--slate-500)";
       spanElement.style.transform = "translate(0%,-50%)";
     } else {
       bodyNode.classList.add("darkMode");
+      darkLightDiv.firstElementChild.firstElementChild.style.fill =
+        "var(--slate-200)";
+      darkLightDiv.lastElementChild.firstElementChild.style.fill =
+        "var(--slate-500)";
       spanElement.style.transform = "translate(-100%,-50%)";
     }
   });
