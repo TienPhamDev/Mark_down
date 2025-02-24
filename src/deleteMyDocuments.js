bb2552e88f426d89f7064795c638ffaf8639d1fa
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-02-24 14:39:30 -0500

    start code modal

--- src/deleteMyDocuments.js
+++ src/deleteMyDocuments.js
@@ -2,9 +2,15 @@ export const deleteMyDocuments = (element) => {
     // Your code here
     const data = JSON.parse(localStorage.getItem("documentsData")) || [];
     element.addEventListener("click", () => {
-        const nameDocument = document.querySelector(".nameDocument");
-        const deleteCurrentDocument = data.filter((item) => item.name !== nameDocument.value);
-        localStorage.setItem("documentsData", JSON.stringify(deleteCurrentDocument));
+        const modal = document.querySelector(".modal");
+        const confirmBtn = document.querySelector(".confirmBtn");
+
+        //turn on modal
+        modal.style.display = "block";
+
+        // const nameDocument = document.querySelector(".nameDocument");
+        // const deleteCurrentDocument = data.filter((item) => item.name !== nameDocument.value);
+        // localStorage.setItem("documentsData", JSON.stringify(deleteCurrentDocument));
         
     }); 
 };
