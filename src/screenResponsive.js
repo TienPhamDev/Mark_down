f2beb1792ccd6ddd03b860525390a48f1c8a1060
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-03-07 14:10:08 -0500

    Update screenResponsive.js

--- src/screenResponsive.js
+++ src/screenResponsive.js
@@ -2,7 +2,7 @@ import { markDownToHTML } from "./markDownToHTML";
 export const screenResponsive = (markDownSection, markDownEditor) => {
   window.addEventListener("resize", function () {
     const width = window.innerWidth;
-    console.log(width);
+    
     const markDownInput = markDownEditor.firstElementChild;
     const markDownOutput = markDownEditor.lastElementChild;
     const markDownHead = markDownSection.firstElementChild;
