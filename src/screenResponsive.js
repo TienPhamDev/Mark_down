682ea53b3c1917da16c8c515afb395793ec2a584
TienPhamDev <manhtienbn18891@gmail.com>
2025-03-06 22:53:00 -0500

    code tablet responsive

--- src/screenResponsive.js
+++ src/screenResponsive.js
@@ -1,21 +1,24 @@
 import { markDownToHTML } from "./markDownToHTML";
 export const screenResponsive = (markDownSection, markDownEditor) => {
-  const width = screen.width;
-  console.log(width);
-  const markDownInput = markDownEditor.firstElementChild;
-  const markDownOutput = markDownEditor.lastElementChild;
-  const markDownHead = markDownSection.firstElementChild;
+  window.addEventListener("resize", function () {
+    const width = window.innerWidth;
+    console.log(width);
+    const markDownInput = markDownEditor.firstElementChild;
+    const markDownOutput = markDownEditor.lastElementChild;
+    const markDownHead = markDownSection.firstElementChild;
+    const markDownTablet = markDownSection.querySelector(
+      ".markDownPreviewTablet"
+    );
 
-  if (width >= 577 && width <= 768) {
-    markDownHead.style.display = "none";
-    markDownEditor.lastElementChild.classList.remove("hidden");
+    if (width >= 577 && width <= 768) {
+      // markDownHead.style.display = "none";
+      // markDownEditor.lastElementChild.classList.remove("hidden");
+      // // markDownTablet.style.display = "flex";
 
-    markDownEditor.style.display = "flex";
-  }
-  markDownInput.addEventListener("input", (event) => {
-    markDownOutput.innerHTML = markDownToHTML(event.target.value);
-    // if (event.key === "Enter") {
-    //   markDownOutput.innerHTML = markDownToHTML(event.target.value);
-    // }
+      // markDownEditor.style.display = "flex";
+      markDownInput.addEventListener("input", (event) => {
+        markDownOutput.innerHTML = markDownToHTML(event.target.value);
+      });
+    }
   });
 };
