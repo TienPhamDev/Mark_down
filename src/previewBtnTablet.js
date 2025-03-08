2766cc767ece9ad8d27285765bbfb97e65d13e72
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-03-08 14:20:54 -0500

    Update previewBtnTablet.js

--- src/previewBtnTablet.js
+++ src/previewBtnTablet.js
@@ -4,20 +4,27 @@ export const previewBtnTablet = (element) => {
   const open = element.firstElementChild;
   const close = element.lastElementChild;
   const markDownEditor = document.querySelector("#markDownEditor")
-
+  
 
   // test window screen size
   const width = window.innerWidth;
+  // state of screen, is it in tablet sreen or not
   const isTablet = width >= 572 && width <= 768 ? true : false;
   
   const onClickBtn = () => {
     open.classList.toggle('hidden');
     close.classList.toggle('hidden');
-    headMarkDown.firstElementChild.classList.toggle('hidden');
-    if(headMarkDown.firstElementChild.classList.contains('hiddens') && isTablet){
-      headMarkDown.style.display = 'block'; 
-      headMarkDown.lastElementChild.style.width ="100%";
-
+    headMarkDown.firstElementChild.classList.toggle('hidden')
+    if(headMarkDown.firstElementChild.classList.contains('hidden') && isTablet){
+      headMarkDown.style.display = "block"
+      headMarkDown.lastElementChild.style.width="100%";
+      headMarkDown.lastElementChild.style.borderLeft="none";
+      headMarkDown.lastElementChild.style.paddingLeft="0";
+    } else{
+      headMarkDown.style.display = "flex"
+      headMarkDown.lastElementChild.style.width="50%";
+      headMarkDown.lastElementChild.style.borderLeft="#2b2d31 2px solid";
+      headMarkDown.lastElementChild.style.paddingLeft="1.6rem";
     }
 
   }
