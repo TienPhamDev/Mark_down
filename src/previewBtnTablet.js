1c214dc2983c5190735d66b24a4239694888f076
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-03-10 00:24:16 -0400

    Update previewBtnTablet.js

--- src/previewBtnTablet.js
+++ src/previewBtnTablet.js
@@ -11,20 +11,31 @@ export const previewBtnTablet = (element) => {
   // state of screen, is it in tablet sreen or not
   const isTablet = width >= 572 && width <= 768 ? true : false;
   
+  // on click function
   const onClickBtn = () => {
     open.classList.toggle('hidden');
     close.classList.toggle('hidden');
     headMarkDown.firstElementChild.classList.toggle('hidden')
+    markDownEditor.firstElementChild.classList.toggle('hidden')
     if(headMarkDown.firstElementChild.classList.contains('hidden') && isTablet){
       headMarkDown.style.display = "block"
       headMarkDown.lastElementChild.style.width="100%";
       headMarkDown.lastElementChild.style.borderLeft="none";
       headMarkDown.lastElementChild.style.paddingLeft="0";
+
+      markDownEditor.style.display = "block";
+      markDownEditor.lastElementChild.style.width = "100%";
+      markDownEditor.lastElementChild.style.borderLeft = "none";
+
     } else{
       headMarkDown.style.display = "flex"
       headMarkDown.lastElementChild.style.width="50%";
       headMarkDown.lastElementChild.style.borderLeft="#2b2d31 2px solid";
       headMarkDown.lastElementChild.style.paddingLeft="1.6rem";
+
+      markDownEditor.style.display = "flex";
+      markDownEditor.lastElementChild.style.width = "50%";
+      markDownEditor.lastElementChild.style.borderLeft = "#2b2d31 2px solid";
     }
 
   }
